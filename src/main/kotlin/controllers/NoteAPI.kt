52b02efc10d37f30f629a574e9c9e5331aa63fee
Zaradin <crottyjj@hotmail.com>
2023-02-20 02:04:02 +0000

    Add list functions for active and Archived notes

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -36,4 +36,74 @@ class NoteAPI {
     }
 
 
+    // method for listing all the active notes
+    /*
+    * First check if the note arraylist is empty or not
+    * Then loop through the ArrayList also checking the isNoteArchived variable
+    * If isNoteArchived is false then add that note to the list that will be returned.
+    * */
+    fun listActiveNotes(): String {
+        return if(notes.isEmpty()){
+            "No notes stored"
+        } else {
+            var listOfActiveNotes = ""
+            for(i in notes.indices){
+                if(!notes[i].isNoteArchived){
+                    listOfActiveNotes += "${i}: ${notes[i]} \n"
+                }
+            }
+            if(listOfActiveNotes.isEmpty()){
+                return "No active notes stored"
+            } else {
+                return listOfActiveNotes
+            }
+        }
+    }
+
+    // method for listing all the Archived notes
+    /*
+    * First check if the note arraylist is empty or not; if so return No notes stored
+    * Then loop through the ArrayList also checking the isNoteArchived variable
+    * If isNoteArchived is true then add that note to the list that will be returned.
+    * */
+    fun listArchivedNotes(): String {
+        return if(notes.isEmpty()){
+            "No notes stored"
+        } else {
+            var listOfArchivedeNotes = ""
+            for(i in notes.indices){
+                if(notes[i].isNoteArchived){
+                    listOfArchivedeNotes += "${i}: ${notes[i]} \n"
+                }
+            }
+            if(listOfArchivedeNotes.isEmpty()){
+                return "No archived notes stored"
+            } else {
+                return listOfArchivedeNotes
+            }
+        }
+    }
+
+    fun numberOfArchivedNotes(): Int {
+        //helper method to determine how many archived notes there are
+        var numOfArchivedNotes: Int = 0
+        for(i in notes.indices){
+            if(notes[i].isNoteArchived){
+                numOfArchivedNotes++
+            }
+        }
+        return numOfArchivedNotes
+    }
+
+    fun numberOfActiveNotes(): Int {
+        //helper method to determine how many active notes there are
+        var numOfActiveNotes: Int = 0
+        for(i in notes.indices){
+            if(!notes[i].isNoteArchived){
+                numOfActiveNotes++
+            }
+        }
+        return numOfActiveNotes
+    }
+
 }
